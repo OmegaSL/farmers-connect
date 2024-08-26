17cb0943bf6aad6e13ef8aa37109a82eeb0347ad
OmegaSL <favournwevo@gmail.com>
2024-08-26 12:30:32 +0000

    feat: finished with coupon module and starting the orders module

---
+++ app/Filament/Resources/CouponResource.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CouponResource\Pages;
+use App\Filament\Resources\CouponResource\RelationManagers;
+use App\Models\Coupon;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CouponResource extends Resource
+{
+    protected static ?string $model = Coupon::class;
+
+    protected static ?string $recordTitleAttribute = 'code';
+
+    protected static ?int $navigationSort = 5;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function getNavigationGroup(): ?string
+    {
+        return __('Order Management');
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                // Forms\Components\TextInput::make('code')
+                //     ->required()
+                //     ->maxLength(255),
+                Forms\Components\Select::make('discount_type')
+                    ->options([
+                        'fixed' => 'Fixed',
+                        'percentage' => 'Percentage',
+                    ])
+                    ->required(),
+                Forms\Components\TextInput::make('discount_value')
+                    ->required()
+                    ->numeric()
+                    ->minValue(0),
+                Forms\Components\DateTimePicker::make('start_date')
+                    ->required(),
+                Forms\Components\DateTimePicker::make('end_date')
+                    ->required(),
+                Forms\Components\Textarea::make('description')
+                    ->columnSpanFull(),
+                Forms\Components\TextInput::make('usage_limit')
+                    ->numeric()
+                    ->minValue(0)
+                    ->default(1),
+                Forms\Components\Toggle::make('active')
+                    ->required()
+                    ->default(true),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('code')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('discount_type'),
+                Tables\Columns\TextColumn::make('discount_value')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('usage_limit')
+                    ->numeric()
+                    ->sortable(),
+                // Tables\Columns\TextColumn::make('start_date')
+                //     ->dateTime()
+                //     ->sortable(),
+                // Tables\Columns\TextColumn::make('end_date')
+                //     ->dateTime()
+                //     ->sortable(),
+                Tables\Columns\IconColumn::make('active')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label(trans('Creation Date'))
+                    ->dateTime('M j, Y')
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCoupons::route('/'),
+            'create' => Pages\CreateCoupon::route('/create'),
+            'edit' => Pages\EditCoupon::route('/{record}/edit'),
+        ];
+    }
+}
