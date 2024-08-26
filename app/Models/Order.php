f9de65b4de523ce0b5641a70745cecaab28a9352
OmegaSL <favournwevo@gmail.com>
2024-08-26 13:46:14 +0000

    fix: added product id column and on  it model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use function PHPUnit\Framework\isEmpty;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-
-use function PHPUnit\Framework\isEmpty;
 
 class Order extends Model
 {
@@ -47,6 +48,16 @@ class Order extends Model
         $trackingNumber = "ORD-{$date}-{$uniqueId}";
 
         return $trackingNumber;
+    }
+
+    /**
+     * Get the user that owns the Order
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 
     /**
